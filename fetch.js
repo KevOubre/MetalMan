80e351b09517cf0dad4e231405865b3c05d9bd45
KevOubre <195364oubre@chsbr.net>
2018-11-13 12:26:13 -0600

    added error handling for javascript

--- fetch.js
+++ fetch.js
@@ -25,52 +25,56 @@ const client = contentful.createClient({
   accessToken: 'dad21cd7010f7eb24533c2f50a8502b1704b30051ebaea6e2ddc0c3b906769f8'
 })
 
+function make_a_file(element) {
+
+  if (element.fields.whoami != undefined) {
+    var stream = fs.createWriteStream("src/html/" + element.fields.whoami + "/" + element.fields.slug + ".md");
+    console.log(element.fields.title)
+    stream.once('open', function (fd) {
+      stream.write("---\n");
+      stream.write("title: " + element.fields.title + " \n");
+      stream.write("date : " + "2016-10-14" + "\n")
+      stream.write("contentful:" + "\n")
+
+      stream.write("  contentype: " + "interview" + "\n")
+      stream.write("  entry_id : " + element.sys.id + " \n")
+      stream.write("exists : " + element.fields.slug + " \n")
+
+      if (element.fields.whoami == "interview") {
+        stream.write("template : post.html \n")
+        stream.write("--- ")
+        if (element.fields.transcripedText != undefined) {
+          stream.write("\n" + element.fields.transcripedText)
+        }
+      } else {
+        stream.write("template : wikientry.html \n")
+        stream.write("---")
+
+      }
+
+      stream.end();
+    })
+  }
+};
 
 
 function make_files(li) {
 
 
-  li.items.forEach(element => {
-    
-    if (element.fields.whoami != undefined) {
-      var stream = fs.createWriteStream("src/html/" + element.fields.whoami + "/" + element.fields.slug + ".md");
-      console.log(element.fields.title)
-      stream.once('open', function (fd) {
-        stream.write("---\n");
-        stream.write("title: " + element.fields.title + " \n");
-        stream.write("date : " + "2016-10-14" + "\n")
-        stream.write("contentful:" + "\n")
+    li.items.forEach(obj => {
+      try {
+        make_a_file(obj)
+      } catch (e) {
+        console.log(e)
+      }
+    })
 
-        stream.write("  contentype: " + "interview" + "\n")
-        stream.write("  entry_id : " + element.sys.id + " \n")
-        stream.write("exists : " + element.fields.slug + " \n")
+  };
 
-        if (element.fields.whoami == "interview") {
-          stream.write("template : post.html \n")
-          stream.write("--- ")
-          if (element.fields.transcripedText != undefined){
-            stream.write("\n" + element.fields.transcripedText)
-          }
-        } else {
-          stream.write("template : wikientry.html \n")
-          stream.write("---")
 
-        }
-
-        stream.end();
-      })
-    };
-
-  });
   //console.log(li.items)
-
-}
-
-
 
 
 client.getEntries()
   .then((response) => make_files(response))
   .catch(console.error)
-
-
